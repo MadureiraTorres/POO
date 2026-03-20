2d0d943f3c97632c1d02949fc83abd8cdd336ce1
Rafael Torres <rafaeltorres.2020@alunos.utfpr.edu.br>
2026-03-20 16:05:18 -0300

    Aula 1

---
+++ Python/Aula 01/ex1.py
@@ -0,0 +1,10 @@
+#Implemente uma classe de ImagemDigital em Python, 
+# adicionando atributos, métodos e faça testes de atribuição e 
+# acesso de valores em uma função principal.
+
+class ImagemDigital:
+    pass
+
+if __name__ == "__main__":
+    foto = ImagemDigital()
+    print(type(foto))
