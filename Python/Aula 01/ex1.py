e8e8ab40c573be4425a37d5ac58161aeea4cdce7
Rafael Torres <rafaeltorres.2020@alunos.utfpr.edu.br>
2026-03-20 16:12:08 -0300

    Update ex1.py

--- Python/Aula 01/ex1.py
+++ Python/Aula 01/ex1.py
@@ -3,7 +3,34 @@
 # acesso de valores em uma função principal.
 
 class ImagemDigital:
-    pass
+    def __init__(self):
+        self._largura = 0
+        self._altura = 0
+        self._nome = ""
+
+    @property
+    def largura(self):
+        return self._largura
+
+    @largura.setter
+    def largura(self, valor):
+        self._largura = valor
+
+    @property
+    def altura(self):
+        return self._altura
+
+    @altura.setter
+    def altura(self, valor):
+        self._altura = valor
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, valor):
+        self._nome = valor
 
 if __name__ == "__main__":
     foto = ImagemDigital()
