8c97549028d695c50465ae996b67904288d50018
GabrielBaruta <65723568+GabrielBaruta@users.noreply.github.com>
2026-03-25 13:23:46 -0300

    Update ClasseJogador.py

--- Python/Projeto/ClasseJogador.py
+++ Python/Projeto/ClasseJogador.py
@@ -1,4 +1,19 @@
 #Projeto: Classe de Jogador de RPG
+#nome;
+#classe;
+#vida; //upa com vitalidade
+#stamina; //upa com resistência ou fortitude, não sei ainda
+#nivel; //nivel aumenta conforme upamos os atributos abaixo
+#almas; //moeda de compra e que também serve para upar os atributos 
+#vitalidade; //quant de hp, quando eu abrir o jogo de novo eu vejo o que de sub-atributo upa com ele
+#conhecimento; //quant de espaços para alocar magias, quando eu abrir o jogo de novo eu vejo o que de sub-atributo upa com ele
+#fortitude; //quant de peso que o jogador pode carregar, responsavel tbm pelo poise, e a stamina é aqui ou na resistencia
+#forca; //quant de dano causado com armas que usam esse atributo, serve como nota de corte pra usar armas de força
+#destreza; //quant de dano causado com armas que usam esse atributo, serve como nota de corte pra usar armas de dex
+#resistencia; //não me lembro se é aqui a stamina ou na fortitude
+#inteligencia; //atributo para usar sorceries/sortilégios, nota de corte pra magias
+#fe; //atributo pra usar milagres (e acho que tem uma piromância que tem dano base de fé)
+
 class Jogador:
 
     def __init__(self):
