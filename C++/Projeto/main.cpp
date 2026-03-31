e8cf88356a281722abdb3d18f7be5c17dc786a07
Rafael Torres <rafaeltorres.2020@alunos.utfpr.edu.br>
2026-03-30 23:29:43 -0300

    Dúvidas para a aula

--- C++/Projeto/main.cpp
+++ C++/Projeto/main.cpp
@@ -1,5 +1,10 @@
 #include "jogador.h"
 
+/*
+Perguntar para o prof:
+-como definir as classes iniciais e se elas vão ser outras classes de fato no código?
+*/
+
 int main(){
     Jogador jogador1("Solaire", "Cavaleiro", 1, 0, 100, 10, 10, 10, 10, 10, 10, 10);
     jogador1.exibirStatus();
