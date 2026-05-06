b872ecc146a7718cf702de61c83adb510107fdcc
Rafael Tores <rafaeltorres.2020@alunos.utfpr.edu.br>
2026-05-06 14:20:09 -0300

    Refactor payment classes and move definitions to pagamento.h

--- C++/Aula 06/Exemplo/main.cpp
+++ C++/Aula 06/Exemplo/main.cpp
@@ -1,41 +1,4 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-
-// Definição de classes
-class IPagamento{
-    public:
-        virtual void processar(float valor) = 0;
-        virtual ~IPagamento(){};
-};
-
-class IAutenticavel{
-    public:
-        virtual void autenticar() = 0;
-        virtual ~IAutenticavel(){};
-};
-
-class Pix : public IPagamento{
-    public:
-        void processar(float valor) override{
-            cout << "Pix: " << valor << endl;
-        }
-};
-
-class Cartao : public IPagamento, public IAutenticavel{
-    public:
-        void processar(float valor) override{
-            //autenticar();
-            cout << "Cartao: " << valor << endl;
-        }
-        void autenticar() override{
-            cout << "Cartao autenticado" << endl;
-        }
-};
-
-//cabeçalho de funções
-void processarPagamento(IPagamento *pagamento, float valor);
+#include "pagamento.h"
 
 //main
 int main(){
@@ -53,9 +16,4 @@ int main(){
     delete(p2);
 
     return 0;
-}
-
-//demais funções
-void processarPagamento(IPagamento *pagamento, float valor){
-    pagamento -> processar(valor);
 }
