9832313c8a972f939ed5b907976709975b63b946
Rafael Tores <rafaeltorres.2020@alunos.utfpr.edu.br>
2026-05-06 14:08:08 -0300

    Refactor payment classes and improve formatting in main.cpp

--- C++/Aula 06/Exemplo/main.cpp
+++ C++/Aula 06/Exemplo/main.cpp
@@ -2,28 +2,41 @@
 #include <string>
 using namespace std;
 
+
+// Definição de classes
 class IPagamento{
     public:
         virtual void processar(float valor) = 0;
         virtual ~IPagamento(){};
 };
 
+class IAutenticavel{
+    public:
+        virtual void autenticar() = 0;
+        virtual ~IAutenticavel(){};
+};
+
 class Pix : public IPagamento{
     public:
         void processar(float valor) override{
-            cout << "Pix:␣" << valor << endl;
+            cout << "Pix: " << valor << endl;
         }
 };
 
-class Cartao : public IPagamento{
+class Cartao : public IPagamento, public IAutenticavel{
     public:
         void processar(float valor) override{
-            cout << "Cartao:␣" << valor << endl;
+            cout << "Cartao: " << valor << endl;
+        }
+        void autenticar() override{
+            cout << "Cartao autenticado" << endl;
         }
 };
 
+//cabeçalho de funções
 void processarPagamento(IPagamento *pagamento, float valor);
 
+//main
 int main(){
     IPagamento *p1 = new Pix();
     processarPagamento(p1, 100);
@@ -32,6 +45,7 @@ int main(){
     return 0;
 }
 
+//demais funções
 void processarPagamento(IPagamento *pagamento, float valor){
     pagamento -> processar(valor);
 }
